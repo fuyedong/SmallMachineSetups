1b1e79df0099b9084841588ced5767dd31292f69
Wayne Xue <xwsoul@gmail.com>
2016-12-24 12:55:26 +0800

    install php phalcon

--- install_php_extensions.sh
+++ install_php_extensions.sh
@@ -6,11 +6,10 @@ source ./libs/root_check.sh
 must_run_as_root
 
 php_extension_install() {
-    ext_name="php_$1"
     file_name=`basename $2`
-    echo -n "Installing $ext_name: "
-    r=`php -m | grep "$1" | xargs`
-    if [ $r == "$1" ]; then
+    echo -n "Installing $1: "
+    p_e_e=`php -m | grep "$1" | xargs`
+    if [ $p_e_e == "$1" ]; then
         echo "existed."
     else
         echo "OK."
@@ -23,10 +22,32 @@ php_extension_install() {
         phpize
         ./configure
         make && make install
-        echo "[$1]
-extension=$ext_name" >> /etc/php/php.ini
+        echo "
+[$1]
+extension=$1.so" >> /etc/php/php.ini
     fi
 }
 php_extension_install memcached http://pecl.php.net/get/memcached-2.1.0.tgz memcached-2.1.0/
 php_extension_install redis http://pecl.php.net/get/redis-2.2.8.tgz redis-2.2.8/
 php_extension_install solr http://pecl.php.net/get/solr-2.4.0.tgz solr-2.4.0/
+
+
+echo -n "Installing phalcon: "
+pe_src=https://github.com/phalcon/cphalcon/archive/phalcon-v2.0.13.tar.gz
+pe_name=`basename $pe_src`
+pe_exists=`php -m | grep phalcon | xargs`
+if [ $pe_exists == "$1" ]; then
+    echo "existed."
+else
+    echo "OK."
+    cd /root/src
+    if [ ! -e pe_name ]; then
+        wget $pe_src
+    fi
+    tar xzf ${pe_name}
+    cd cphalcon-phalcon-v2.0.13/build/
+    ./build
+    echo "
+[$1]
+extension=phalcon.so" >> /etc/php/php.ini
+fi
