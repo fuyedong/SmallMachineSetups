d1a13763773cca1d7858ac9c5651ba0c9606feba
Wayne Xue <xwsoul@gmail.com>
2016-12-24 11:15:15 +0800

    fix setup php

--- setup_php.sh
+++ setup_php.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ -e "/usr/local/php/bin/php" ]; then
-    if [ !-e "/usr/local/bin/php" ]; then
+    if [ ! -e "/usr/local/bin/php" ]; then
         echo "Making php alias"
         cd /usr/local/bin/
         sudo ln -s /usr/local/php/bin/* .
@@ -21,25 +21,25 @@ if [ ! -e "/ect/php/php.ini" ]; then
         sudo mkdir /etc/php
     fi
     PHP_SRC_DIR=`find /root/src/ -name 'php-5*' -type d`
-    cd "$PHP_SRC_DIR"
-    if [ ! -e "./php.ini-production" ]; then
+    cd ""
+    if [ ! -e "${PHP_SRC_DIR}/php.ini-production" ]; then
         echo "Origin PHP configuration not found."
     else
-        sudo cp "./php.ini-production /etc/php/php.ini"
+        sudo cp "${PHP_SRC_DIR}/php.ini-production /etc/php/php.ini"
     fi
-    if [ ! -e "./sapi/fpm/php-fpm.conf" ]; then
+    if [ ! -e "${PHP_SRC_DIR}/sapi/fpm/php-fpm.conf" ]; then
         echo "Origin PHP-FPM configuration not found."
     else
-        sudo cp "./sapi/fpm/php-fpm.conf /etc/php/php-fpm.conf"
+        sudo cp "${PHP_SRC_DIR}/sapi/fpm/php-fpm.conf /etc/php/php-fpm.conf"
     fi
 fi
 
 if [ ! -e "/etc/init.d/php-fpm" ]; then
-    cd `dirname $0`
-    if [ ! -e "./assets/php-fpm" ]; then
+    PHP_INIT_FILE=`dirname $0`
+    if [ ! -e "${PHP_INIT_FILE}/assets/php-fpm" ]; then
         echo "Origin php-fpm startup script not found."
     else
-        sudo cp "./assets/php-fpm /etc/init.d/php-fpm"
+        sudo cp "${PHP_INIT_FILE}/assets/php-fpm /etc/init.d/php-fpm"
         sudo chmod 0755 /etc/init.d/php-fpm
         cd /etc/rc2.d
         sudo ln -s ../init.d/php-fpm S10php-fpm
